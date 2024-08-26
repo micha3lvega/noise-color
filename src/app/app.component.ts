267483bea0a9a53ac4da69f87b90b8f9a354d481
Michael Vega <micha3lvega@gmail.com>
2024-08-26 18:35:32 +0100

    Crear metodos para dibujar

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,6 +1,13 @@
 import { AudioService } from './audio/audio.service';
-import { Component, OnInit } from '@angular/core';
+import {
+  Component,
+  ElementRef,
+  OnDestroy,
+  OnInit,
+  ViewChild,
+} from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { Circle } from './circle';
 
 @Component({
   selector: 'app-root',
@@ -10,12 +17,32 @@ import { RouterOutlet } from '@angular/router';
   styleUrl: './app.component.css',
 })
 export class AppComponent implements OnInit {
-  title = 'noise-color';
-  constructor(private audioService: AudioService) {}
-  ngOnInit(): void {
-    console.log('Iniciando [AppComponent]');
+  @ViewChild('canvas', { static: true })
+  canvasRef!: ElementRef<HTMLCanvasElement>;
 
+  private ctx!: CanvasRenderingContext2D;
+
+  private animationFrameId!: number;
+
+  circles: Circle[] = [];
+  totalCirclesCreated: number = 0; // Contador de círculos creados desde el inicio
+
+  MAX_CIRCLES: number = 100;
+  MAX_TIME_FADE_OUT_MILIS: number = 3000;
+  MIN_TIME_FADE_OUT_MILIS: number = 1500;
+  MIN_VOLUME_SOUND: number = 1;
+
+  constructor(private audioService: AudioService) {}
+
+  ngOnInit(): void {
+    this.ctx = this.canvasRef.nativeElement.getContext('2d')!;
+    this.canvasRef.nativeElement.width = window.innerWidth;
+    this.canvasRef.nativeElement.height = window.innerHeight;
     this.startMicrophone();
+  }
+
+  ngOnDestroy(): void {
+    cancelAnimationFrame(this.animationFrameId);
   }
 
   startMicrophone(): void {
@@ -24,13 +51,19 @@ export class AppComponent implements OnInit {
     });
   }
 
+  stopMicrophone(): void {
+    this.audioService.stopMicrophone();
+    cancelAnimationFrame(this.animationFrameId);
+  }
+
   private listenToAudio(): void {
     const analyser = this.audioService.getAnalyser();
     const dataArray = this.audioService.getDataArray();
     console.log('Dibujando...');
 
-    setInterval(() => {
-      console.log('dibujando...');
+    const draw = () => {
+
+      console.log('Dibujando...');
 
       analyser.getByteTimeDomainData(dataArray);
 
@@ -39,7 +72,80 @@ export class AppComponent implements OnInit {
         sum += (dataArray[i] - 128) ** 2;
       }
       const volume = Math.sqrt(sum / dataArray.length);
-      console.log('Volume: ', volume);
-    }, 100);
+
+      if (volume > this.MIN_VOLUME_SOUND) {
+        this.createCircle(volume);
+      }
+
+      this.updateCircles();
+      this.animationFrameId = requestAnimationFrame(draw);
+    };
+
+    draw();
+  }
+
+  private createCircle(volume: number): void {
+    if (this.circles.length >= this.MAX_CIRCLES) {
+      this.circles.shift();
+    }
+
+    const canvas = this.canvasRef.nativeElement;
+    const maxSize = Math.max(canvas.width, canvas.height);
+    const initialRadius = volume * (maxSize / 200);
+    const x = Math.random() * canvas.width;
+    const y = Math.random() * canvas.height;
+    const color = `hsla(${Math.random() * 360}, 100%, 50%, 0.8)`;
+    const startTime = Date.now();
+    const fadeOutTime =
+      Math.random() *
+        (this.MAX_TIME_FADE_OUT_MILIS - this.MIN_TIME_FADE_OUT_MILIS) +
+      this.MIN_TIME_FADE_OUT_MILIS;
+
+    const newCircle: Circle = {
+      x,
+      y,
+      radius: initialRadius,
+      color,
+      startTime,
+      fadeOutTime,
+    };
+    this.circles.push(newCircle);
+    this.totalCirclesCreated++; // Incrementar el contador de círculos creados
+  }
+
+  private updateCircles(): void {
+    const canvas = this.canvasRef.nativeElement;
+    const ctx = this.ctx;
+    const currentTime = Date.now();
+
+    ctx.clearRect(0, 0, canvas.width, canvas.height);
+
+    this.circles.forEach((circle, index) => {
+      const timeElapsed = currentTime - circle.startTime;
+      const alpha = Math.max(1 - timeElapsed / circle.fadeOutTime, 0);
+      const radius = circle.radius * alpha;
+
+      if (alpha <= 0) {
+        this.circles.splice(index, 1);
+      } else {
+        // Dibujar círculo externo
+        ctx.beginPath();
+        ctx.arc(circle.x, circle.y, radius, 0, 2 * Math.PI);
+        ctx.strokeStyle = circle.color;
+        ctx.globalAlpha = alpha;
+        ctx.lineWidth = 2;
+        ctx.stroke();
+        ctx.globalAlpha = 1;
+
+        // Dibujar círculo interno más pequeño
+        const innerRadius = radius * 0.8; // Tamaño del círculo interno (ajustable según preferencias)
+        ctx.beginPath();
+        ctx.arc(circle.x, circle.y, innerRadius, 0, 2 * Math.PI);
+        ctx.fillStyle = circle.color;
+        ctx.globalAlpha = alpha * 0.5; // Opacidad reducida para el círculo interno
+        ctx.fill();
+        ctx.globalAlpha = 1;
+      }
+    });
   }
 }
--- src/app/audio/audio.service.ts
+++ src/app/audio/audio.service.ts
@@ -1,67 +1,57 @@
-import { Injectable, OnDestroy } from '@angular/core';
+import { Injectable } from '@angular/core';
 
 @Injectable({
   providedIn: 'root',
 })
-export class AudioService implements OnDestroy {
+export class AudioService {
+    private audioContext!: AudioContext;
+    private mediaStream!: MediaStream | null;
+    private mediaStreamSource!: MediaStreamAudioSourceNode | null;
+    private analyser!: AnalyserNode;
+    private dataArray!: Uint8Array;
 
-  private audioContext!: AudioContext;
-  private mediaStream!: MediaStream | null;
-  private mediaStreamSource!: MediaStreamAudioSourceNode | null;
-  private analyser!: AnalyserNode;
-  private dataArray!: Uint8Array;
-
-  constructor() {
-    this.audioContext = new (window.AudioContext ||
-      (window as any).webkitAudioContext)();
-    this.mediaStream = null;
-    this.mediaStreamSource = null;
-    this.analyser = this.audioContext.createAnalyser();
-    this.analyser.fftSize = 2048;
-    const bufferLength = this.analyser.frequencyBinCount;
-    this.dataArray = new Uint8Array(bufferLength);
-  }
-
-  async getMicrophoneAccess(): Promise<void> {
-    try {
-      this.mediaStream = await navigator.mediaDevices.getUserMedia({
-        audio: true,
-      });
-      this.mediaStreamSource = this.audioContext.createMediaStreamSource(
-        this.mediaStream
-      );
-      this.mediaStreamSource.connect(this.analyser);
-      console.log('Microphone access granted');
-    } catch (err) {
-      console.error('Error accessing microphone:', err);
-    }
-  }
-
-  getAnalyser(): AnalyserNode {
-    return this.analyser;
-  }
-
-  getDataArray(): Uint8Array {
-    return this.dataArray;
-  }
-
-  stopMicrophone(): void {
-    if (this.mediaStream) {
-      this.mediaStream.getTracks().forEach((track) => track.stop());
+    constructor() {
+      this.audioContext = new (window.AudioContext ||
+        (window as any).webkitAudioContext)();
       this.mediaStream = null;
-    }
-    if (this.mediaStreamSource) {
-      this.mediaStreamSource.disconnect();
       this.mediaStreamSource = null;
+      this.analyser = this.audioContext.createAnalyser();
+      this.analyser.fftSize = 2048;
+      const bufferLength = this.analyser.frequencyBinCount;
+      this.dataArray = new Uint8Array(bufferLength);
     }
-  }
 
-  ngOnDestroy(): void {
-    if (this.mediaStreamSource) {
-      this.mediaStreamSource.disconnect();
-      this.audioContext.close();
+    async getMicrophoneAccess(): Promise<void> {
+      try {
+        this.mediaStream = await navigator.mediaDevices.getUserMedia({
+          audio: true,
+        });
+        this.mediaStreamSource = this.audioContext.createMediaStreamSource(
+          this.mediaStream
+        );
+        this.mediaStreamSource.connect(this.analyser);
+        console.log('Microphone access granted');
+      } catch (err) {
+        console.error('Error accessing microphone:', err);
+      }
     }
-    console.log('Close...');
 
-  }
+    getAnalyser(): AnalyserNode {
+      return this.analyser;
+    }
+
+    getDataArray(): Uint8Array {
+      return this.dataArray;
+    }
+
+    stopMicrophone(): void {
+      if (this.mediaStream) {
+        this.mediaStream.getTracks().forEach((track) => track.stop());
+        this.mediaStream = null;
+      }
+      if (this.mediaStreamSource) {
+        this.mediaStreamSource.disconnect();
+        this.mediaStreamSource = null;
+      }
+    }
 }
